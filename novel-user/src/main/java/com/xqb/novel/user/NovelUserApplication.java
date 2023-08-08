94110b64904da735d9fc7912b638c3c042801f8d
dabiao <xieqingbiao05@gamil.com>
2023-08-08 15:44:17 +0800

    项目初始化结构

---
+++ novel-user/src/main/java/com/xqb/novel/user/NovelUserApplication.java
@@ -0,0 +1,13 @@
+package com.xqb.novel.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelUserApplication.class, args);
+    }
+
+}
